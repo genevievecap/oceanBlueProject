baf6fdf4b84bdb77738dedd3271dd1e21aa696c5
gcapolongo <gcapolongo@wisc.edu>
2022-08-29 20:37:43 -0400

    finished jquery

---
+++ index.js
@@ -0,0 +1,11 @@
+$(".donate").on("click", function() {
+    $(this).toggleClass("donate donate1");
+});
+
+$(".shop").on("click", function() {
+    $(this).toggleClass("shop shop1");
+});
+
+$(".clickable").on("click", function() {
+    $(this).toggleClass("clickable clickable1");
+});
